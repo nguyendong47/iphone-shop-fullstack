422813eef4d5be3f56d073a97c2bb8b3eed5dc86
nguyen van dong <dongnguyenvan.it@gmail.com>
2023-09-18 00:25:29 +0700

    Implement payment.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,26 +1,36 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const {
+	register,
+	login,
+	verifyEmail,
+	forgotPassword,
+	resetPassword,
+	createUser,
+	getUsers,
+	updateUser,
+	deleteUser,
+} = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
 // Route để xử lý xác thực email
-router.get('/verify/:token', authController.verifyEmail);
+router.get('/verify/:token', verifyEmail);
 // Route để xử lý yêu cầu đặt lại mật khẩu
-router.post('/forgot-password', authController.forgotPassword);
+router.post('/forgot-password', forgotPassword);
 // Route để xử lý việc người dùng đặt lại mật khẩu
-router.post('/reset-password/:token', authController.resetPassword);
+router.post('/reset-password/:token', resetPassword);
 
 // Route để tạo mới người dùng
-router.post('/users', authController.createUser);
+router.post('/users', createUser);
 
 // Route để lấy danh sách người dùng
-router.get('/users', authController.getUsers);
+router.get('/users', getUsers);
 
 // Route để cập nhật thông tin người dùng
-router.put('/users/:id', authController.updateUser);
+router.put('/users/:id', updateUser);
 
 // Route để xóa người dùng
-router.delete('/users/:id', authController.deleteUser);
+router.delete('/users/:id', deleteUser);
 
 module.exports = router;
