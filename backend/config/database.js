4a6167bccd07dc645ad06a42873f98ff8d0675d0
nguyen van dong <dongnguyenvan.it@gmail.com>
2023-09-17 21:45:34 +0700

    Connect MongoDB.

---
+++ backend/config/database.js
@@ -0,0 +1,30 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoURI = process.env.MONGODB_URI;
+const uri = `mongodb+srv://nguyendong:${mongoURI}@cluster0.hr9qn.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+console.log('mongoDB: ', mongoURI);
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+async function connectDB() {
+	try {
+		console.log('Connecting....');
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		await client.db('iphone').command({ ping: 1 });
+		console.log(
+			'Pinged your deployment. You successfully connected to MongoDB!'
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		await client.close();
+	}
+}
+
+module.exports = connectDB;
---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+
+const connectDB = require('./config/database');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+connectDB().catch(console.dir);
+// Middleware
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Welcome to iPhone Shop Backend!');
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
