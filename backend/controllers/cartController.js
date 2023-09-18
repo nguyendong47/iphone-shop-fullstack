7bf451c3c32b6acfec3360f4a64da1c746f134ef
nguyen van dong <dongnguyenvan.it@gmail.com>
2023-09-18 22:54:32 +0700

    Implement Cart feature.

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,54 @@
+exports.addToCart = async (req, res) => {
+	try {
+		const userId = req.user.id;
+		const { productId, quantity } = req.body;
+
+		let cart = await Cart.findOne({ user: userId });
+
+		if (!cart) {
+			cart = await Cart.create({
+				user: userId,
+				products: [{ product: productId, quantity }],
+			});
+		} else {
+			const productIndex = cart.products.findIndex(
+				(p) => p.product.toString() === productId
+			);
+			if (productIndex > -1) {
+				cart.products[productIndex].quantity += quantity;
+			} else {
+				cart.products.push({ product: productId, quantity });
+			}
+			await cart.save();
+		}
+
+		res.status(200).json({
+			status: 'success',
+			data: cart,
+		});
+	} catch (error) {
+		res.status(500).json({
+			status: 'error',
+			message: 'Lỗi khi thêm sản phẩm vào giỏ hàng.',
+		});
+	}
+};
+
+exports.getCart = async (req, res) => {
+	try {
+		const userId = req.user.id;
+		const cart = await Cart.findOne({ user: userId }).populate(
+			'products.product'
+		);
+
+		res.status(200).json({
+			status: 'success',
+			data: cart,
+		});
+	} catch (error) {
+		res.status(500).json({
+			status: 'error',
+			message: 'Lỗi khi lấy giỏ hàng.',
+		});
+	}
+};
---
+++ backend/routes/cartRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cartController = require('../controllers/cartController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+router.use(authMiddleware.protect);
+
+router.post('/add-to-cart', cartController.addToCart);
+router.get('/get-cart', cartController.getCart);
+
+module.exports = router;
